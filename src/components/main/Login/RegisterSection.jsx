ad3ed7bd60ae6f85d61aa5159d8fdf039e163492
RookieAND <gwangin1999@naver.com>
2022-08-02 22:35:24 +0900

    Feat : #5 - 로그인 화면 1차 시안 최종 적용
    회원가입 버튼과 우측의 카드 섹션 레이아웃을 변경하였습니다.

--- src/components/main/Login/RegisterSection.jsx
+++ src/components/main/Login/RegisterSection.jsx
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom"
 import styled, { css } from "styled-components"
 
 import cardImg from "assets/img/logincard2.jpg"
+import { fadeIn } from "styles/Animation"
 
 const RegisterSection = () => {
   return (
     <Wrapper>
-      {/* <p>아직 회원이 아니시군요?</p>
-      <RegisterBtn to="/signup">회원가입하기</RegisterBtn> */}
+      <h5>Aren't you Join yet?</h5>
+      <RegisterContent>
+        <p>아직 회원이 아니신가요?</p>
+        <RegisterBtn to="/signup">회원가입</RegisterBtn>
+      </RegisterContent>
     </Wrapper>
   )
 }
@@ -22,13 +26,37 @@ const Wrapper = styled.div`
 
       border-radius: 0px 25px 25px 0px;
 
-      background-image: linear-gradient(rgba(0, 76, 9, 0.1), rgba(0, 62, 9, 0.05)), url(${cardImg});
+      background-image: linear-gradient(rgba(0, 18, 2, 0.4), rgba(0, 50, 8, 0.1)), url(${cardImg});
       background-size: cover;
       text-align: center;
 
+      animation: ${fadeIn} 2s 0s;
+      animation-fill-mode: forwards;
+
+      h5 {
+        position: relative;
+        top: 37.5%;
+
+        margin: 0vw auto;
+
+        color: ${colors.white};
+        font-size: ${fonts.size.xxl};
+        font-weight: bold;
+      }
+    `
+  }}
+`
+
+const RegisterContent = styled.div`
+  ${({ theme }) => {
+    const { colors, fonts, margins } = theme
+    return css`
+      position: relative;
+      top: 38.5%;
       p {
-        margin: ${margins.sm} 0vw;
-        color: #000000;
+        margin: ${margins.base} auto;
+
+        color: ${colors.white};
         font-size: ${fonts.size.xsm};
         font-weight: 100;
       }
@@ -41,13 +69,14 @@ const RegisterBtn = styled(Link)`
     const { colors, fonts, paddings } = theme
     return css`
       width: 100%;
-      padding: ${paddings.sm} ${paddings.base};
+      padding: ${paddings.sm} 7.5vw;
 
-      background-color: #000000;
-      border-radius: 10px;
+      background-color: #ffffff;
+      border-radius: 100px;
 
-      color: ${colors.white};
+      color: #000000;
       font-size: ${fonts.size.sm};
+      font-weight: 700;
     `
   }}
 `
