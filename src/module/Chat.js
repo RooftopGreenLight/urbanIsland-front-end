939092d672806fbb8eae10bcbf5b65edf235dbf9
RookieAND <gwangin1999@naver.com>
2022-08-04 19:02:43 +0900

    Feat : StompJS 관련 모듈 추가
    채팅방 기능 구현을 위한 StompJS 라이브러리 및 관련 모듈 섹션을 추가하였습니다.

---
+++ src/module/Chat.js
@@ -0,0 +1,47 @@
+import SockJS from "sockjs-client"
+import { Client } from "stompjs"
+
+// 서버 간 연결을 위해 WebSocket과 통신하는 Client 객체 생성
+export const stompClient = new Client({
+  brokerURL: "ws://localhost:15674/ws",
+  connectHeaders: {
+    login: "user",
+    passcode: "password",
+  },
+
+  debug: function (log) {
+    console.log(log)
+  },
+
+  reconnectDelay: 5000,
+  heartbeatIncoming: 4000,
+  heartbeatOutgoing: 4000,
+})
+
+// Client 가 성공적으로 서버와 연결되었을 때 실행될 함수
+// 재연결 시에도 실행되는 콜백 함수이며, subscribe도 이곳에서 진행.
+stompClient.onConnect = function (frame) {
+  console.log("successfully connected!")
+  const subscription = stompClient.subscribe("chat/general", msg => {
+    const payload = JSON.parse(msg.body)
+    console.log(payload.user, payload.message)
+  })
+}
+
+// Client가 안전하게 접속을 끊었을 경우 실행될 함수.
+// 통신 과정에서의 에러로 끊어진 경우에는 호출되지 않음.
+stompClient.onDisconnect = function (frame) {
+  console.log("successfully disconnected!")
+}
+
+// 통신 도중 Stomp 에러 발생 시 실행될 함수.
+stompClient.onStompError = function (frame) {
+  console.log("Broker reported error: " + frame.headers["message"])
+  console.log("Additional details: " + frame.body)
+}
+
+// Client 활성화
+// client.activate()
+
+// Client 비활성화 및 재연결 종료
+// client.deactivate()
