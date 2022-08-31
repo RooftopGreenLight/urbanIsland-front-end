f0b6ab9f82ac63238ad7f6f320e167449ec4f8c2
RookieAND <gwangin1999@naver.com>
2022-08-31 23:26:56 +0900

    Feat : KakaoPay 테스트 결제 API 기능 추가
    카카오 페이 테스트 결제를 진행할 수 있는 API를 추가했습니다.

---
+++ src/api/KakaoPay.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+
+export const KakaoPayControl = {
+  postRequestToPay: async () => {
+    let response
+    console.log(1)
+    try {
+      response = await axios({
+        baseURL: process.env.REACT_APP_KAKAO_PAY_HOST_URL,
+        url: "/v1/payment/ready",
+        method: "POST",
+        headers: {
+          Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_ADMIN_KEY}`,
+          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+        params: {
+          cid: "TC0ONETIME",
+          partner_order_id: "partner_order_id",
+          partner_user_id: "partner_user_id",
+          item_name: "테스트숙소예약",
+          quantity: 1,
+          total_amount: 22000,
+          vat_amount: 0,
+          tax_free_amount: 0,
+          approval_url: "http://localhost:3000/mypage/rooftop",
+          fail_url: "http://localhost:3000",
+          cancel_url: "http://localhost:3000",
+        },
+      })
+      return response.data
+    } catch (err) {
+      console.log(err)
+      throw new Error(err)
+    }
+  },
+}
