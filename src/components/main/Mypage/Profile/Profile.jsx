5e16ce7208e0da9f8a6d2d752283b20655d90f70
RookieAND <gwangin1999@naver.com>
2022-09-13 01:34:35 +0900

    Fix : 마이페이지 메뉴 권한 별 활성화 코드 2차 수정
    마이페이지 메뉴 별 권한 활성화 관련 코드를 추가 수정하고, Admin 권한에 따른 토글 여부를 추가했습니다.

--- src/components/main/Mypage/Profile/Profile.jsx
+++ src/components/main/Mypage/Profile/Profile.jsx
@@ -84,7 +84,7 @@ const Profile = () => {
           <h5>서비스 등록하기</h5>
         </Title>
 
-        {authority === "ROLE_ALL" || "ROLE_GREENBEE" ? (
+        {["ROLE_ADMIN", "ROLE_ALL", "ROLE_GREENBEE"].includes(authority) ? (
           <ServiceBox>
             <div className="introduce">
               <h5>그린비 등록하기</h5>
@@ -93,17 +93,17 @@ const Profile = () => {
             <FontAwesomeIcon icon={faCircleCheck} />
           </ServiceBox>
         ) : (
-          <ServiceBox onClick={() => openModal(<ApplyModal />)}>
-            <Link to="/mypage/greenbee/register">
+          <Link to="/mypage/greenbee/register">
+            <ServiceBox>
               <div className="introduce">
                 <h5>그린비 등록하기</h5>
                 <p>"조경이 가능한 건축사무소를 운영중이신가요?</p>
               </div>
               <FontAwesomeIcon icon={faAngleRight} />
-            </Link>
-          </ServiceBox>
+            </ServiceBox>
+          </Link>
         )}
-        {authority === "ROLE_ALL" || "ROLE_ROOFTOPOWNER" ? (
+        {["ROLE_ADMIN", "ROLE_ALL", "ROLE_ROOFTOPOWNER"].includes(authority) ? (
           <ServiceBox>
             <div className="introduce">
               <h5>옥상지기 등록하기</h5>
