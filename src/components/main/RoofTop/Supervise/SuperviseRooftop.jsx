c0e8f31bc35ee4f34209178f5905348ab99e52fb
JunginO <hardertobreathe@naver.com>
2022-09-12 23:43:47 +0900

    Fix: 11 레이아웃 변경

--- src/components/main/RoofTop/Supervise/SuperviseRooftop.jsx
+++ src/components/main/RoofTop/Supervise/SuperviseRooftop.jsx
@@ -1,7 +1,6 @@
 import { Link, useParams } from "react-router-dom"
 import styled, { css } from "styled-components"
 import { CalenderContainer } from "styles/calender"
-import defaultProfile from "assets/img/defaultProfile.png"
 import Calendar from "react-calendar"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState, useEffect } from "react"
@@ -17,144 +16,112 @@ import {
 import "react-calendar/dist/Calendar.css"
 const SuperviseRooftop = () => {
   const { id } = useParams()
-  const [input, setInput] = useState({
-    mainImage: [],
-    adultCount: 0,
-    kidCount: 0,
-    petCount: 0,
-    totalCount: 0,
-    startTime: [],
-    endTime: [],
-    totalPrice: 0,
-    deleteImages: [],
-    addImages: [],
-    rooftopImages: [],
-    structureImage: [],
-  })
-
-  const { mainImage, totalCount, deleteImages, addImages, rooftopImages, structureImage } = input
+  const [data, setData] = useState({ mainImage: [], city: "", district: "", detail: "" })
 
   useEffect(() => {
     const loadCurrentInfo = async () => {
-      const {
-        mainImage,
-        structureImage,
-        rooftopImages,
-        kidCount,
-        adultCount,
-        petCount,
-        totalCount,
-        totalPrice,
-        startTime,
-        endTime,
-      } = await roofTopControl.getRooftopDetail(id)
-      setInput({
-        ...input,
-        rooftopImages: rooftopImages.filter(
-          ({ rooftopImageType }) => rooftopImageType === "NORMAL",
-        ),
-        structureImage,
-        kidCount,
-        adultCount,
-        petCount,
-        totalCount,
-        totalPrice,
-        startTime,
-        endTime,
-        mainImage,
-      })
+      try {
+        const result = await roofTopControl.getRooftopDetail(id)
+        const { mainImage, city, district, detail } = result
+        setData({ mainImage, city, district, detail })
+      } catch (err) {
+        console.log(err.message)
+      }
     }
     loadCurrentInfo()
   }, [])
+  const { mainImage, city, district, detail } = data
   return (
     <Wrapper>
+      {data && (
+        <Maintitle>
+          {city} {district} {detail} 옥상
+        </Maintitle>
+      )}
       <ViewPoint>
-        <UpperBox>
-          <SideBox>
-            {mainImage && <Image src={mainImage.fileUrl} />}
-            <BoxWrapper>
-              <Title>예약내역</Title>
-              <ScheduleDetail>
-                <p>
-                  <FontAwesomeIcon icon={faBuilding} size="lg" />
-                  예약 숙소
-                </p>
-                <span>경기도 화성시 시범중앙로 109 320동 601호</span>
-              </ScheduleDetail>
-              <ScheduleDetail>
-                <p>
-                  <FontAwesomeIcon icon={faCalendar} size="lg" />
-                  예약일자
-                </p>
-                <span>2022.9.22 - 2022.9.23</span>
-              </ScheduleDetail>
-              <ScheduleDetail>
-                <p>
-                  <FontAwesomeIcon icon={faClock} size="lg" />
-                  예약시간
-                </p>
-                <span>AM 11:00 ~ PM 8:00</span>
-              </ScheduleDetail>
-              <ScheduleDetail>
-                <p>
-                  <FontAwesomeIcon icon={faUser} size="lg" /> 총 인원
-                </p>
-                <span>성인 2명, 유아 1명 (반려동물 1마리)</span>
-              </ScheduleDetail>
-            </BoxWrapper>
-            <BoxWrapper>
-              <Title>채팅내역</Title>
-              <Button>
-                <FontAwesomeIcon icon={faComments} /> 채팅 목록 열기
-              </Button>
-            </BoxWrapper>
-            <BoxWrapper>
-              {" "}
-              <Link to={`/mypage/rooftop/supervise/detail/${id}`}>
-                <Button>내 옥상 수정하기</Button>
-              </Link>
-            </BoxWrapper>
-          </SideBox>
-          <SideBox>
-            <BoxWrapper>
-              <CalenderContainer>
-                <Calendar />
-              </CalenderContainer>
-            </BoxWrapper>
-            <BoxWrapper>
-              <Title>문의 알림란</Title>
-              <InnerBox>
-                <InnerTop>
-                  <InnerTitle>문의</InnerTitle>
-                  <div>22.07.20 11:20</div>
-                </InnerTop>
-                <InnerMessage>
-                  혹시 애견인 모임으로 활용하려 하는데 xx개의 좌석 확보할 수 있을까요?
-                </InnerMessage>
-              </InnerBox>
-              <InnerBox>
-                <InnerTop>
-                  <InnerTitle>문의</InnerTitle>
-                  <div>22.07.20 11:20</div>
-                </InnerTop>
-                <InnerMessage>
-                  혹시 애견인 모임으로 활용하려 하는데 xx개의 좌석 확보할 수 있을까요?
-                </InnerMessage>
-              </InnerBox>
-            </BoxWrapper>
-            <BoxWrapper>
-              <Title>조경업체</Title>
-              <div>서울특별시 동구 테스트 주소 어쩌구</div>
-              <div>전화번호: 010-1111-2222</div>
-            </BoxWrapper>
-          </SideBox>
-        </UpperBox>
-        <BottomBox>
+        <Title>대표이미지</Title>
+        {mainImage && <Image src={mainImage.fileUrl} />}
+        <BoxWrapper>
+          <CalenderContainer>
+            <Title>캘린더</Title>
+            <Calendar />
+          </CalenderContainer>
+        </BoxWrapper>
+        <BoxWrapper>
+          <Title>예약내역</Title>
+          <ScheduleDetail>
+            <p>
+              <FontAwesomeIcon icon={faBuilding} size="lg" />
+              예약 숙소
+            </p>
+            <span>경기도 화성시 시범중앙로 109 320동 601호</span>
+          </ScheduleDetail>
+          <ScheduleDetail>
+            <p>
+              <FontAwesomeIcon icon={faCalendar} size="lg" />
+              예약일자
+            </p>
+            <span>2022.9.22 - 2022.9.23</span>
+          </ScheduleDetail>
+          <ScheduleDetail>
+            <p>
+              <FontAwesomeIcon icon={faClock} size="lg" />
+              예약시간
+            </p>
+            <span>AM 11:00 ~ PM 8:00</span>
+          </ScheduleDetail>
+          <ScheduleDetail>
+            <p>
+              <FontAwesomeIcon icon={faUser} size="lg" /> 총 인원
+            </p>
+            <span>성인 2명, 유아 1명 (반려동물 1마리)</span>
+          </ScheduleDetail>
+        </BoxWrapper>
+        <BoxWrapper>
+          <Title>채팅내역</Title>
+          <Button>
+            <FontAwesomeIcon icon={faComments} /> 채팅 목록 열기
+          </Button>
+        </BoxWrapper>
+        <BoxWrapper>
+          <Title>문의 알림란</Title>
+          <InnerBox>
+            <InnerTop>
+              <p>문의</p>
+              <span>22.07.20 11:20</span>
+            </InnerTop>
+            <span>
+              혹시 애견인 모임으로 활용하려 하는데 xx개의 좌석 확보할 수 있을까요?혹시 애견인
+              모임으로 활용하려 하는데 xx개의 좌석 확보할 수 있을까요?
+            </span>
+          </InnerBox>
+          <InnerBox>
+            <InnerTop>
+              <p>문의</p>
+              <span>22.07.20 11:20</span>
+            </InnerTop>
+            <span> 혹시 애견인 모임으로 활용하려 하는데 xx개의 좌석 확보할 수 있을까요?</span>
+          </InnerBox>
+        </BoxWrapper>
+        <BoxWrapper>
+          <Title>조경업체</Title>
+          <p>서울특별시 동구 테스트 주소 어쩌구</p>
+          <span>전화번호: 010-1111-2222</span>
+        </BoxWrapper>
+        <BoxWrapper>
           <Title>수수료 산정 공지</Title>
           <p>현재 옥상에 대한 수수료 10%</p>
-          수수료 재산정의 경우 어쩌구 저쩌구
-          <Button>수수료 재산정 요청</Button>
-        </BottomBox>
+          <span>
+            수수료 재산정의 경우 어쩌구
+            ㅁㄴㅇㅁㄴㅇㅁㄴㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㅇㄴㄴㅇㅁㅇㅁㄴㅇㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+          </span>
+          <Button type="fee">수수료 재산정 요청</Button>
+        </BoxWrapper>
+        <BoxWrapper>
+          <Button>
+            <Link to={`/mypage/rooftop/supervise/detail/${id}`}>내 옥상 수정하기</Link>
+          </Button>
+        </BoxWrapper>
       </ViewPoint>
     </Wrapper>
   )
@@ -169,7 +136,8 @@ const Button = styled.div`
 
       cursor: pointer;
       border-radius: ${fonts.size.sm};
-      background-color: ${colors.main.primary};
+      background-color: ${props =>
+        props.type === "fee" ? colors.main.tertiary : colors.main.primary};
 
       text-align: center;
       color: ${colors.white};
@@ -189,26 +157,12 @@ const Button = styled.div`
 
 const ScheduleDetail = styled.div`
   ${({ theme }) => {
-    const { colors, fonts, paddings, margins } = theme
+    const { colors, paddings, margins } = theme
     return css`
       display: flex;
       justify-content: space-between;
       padding: ${paddings.sm};
       color: ${colors.black.secondary};
-
-      span {
-        width: 55%;
-        font-weight: 300;
-        text-align: right;
-      }
-
-      p {
-        width: 40%;
-        margin: ${margins.xsm} 0vw;
-        font-size: ${fonts.size.xsm};
-        font-weight: ${fonts.weight.light};
-        text-align: left;
-      }
 
       svg {
         color: ${colors.main.primary};
@@ -222,6 +176,9 @@ const Wrapper = styled.div`
   margin: auto;
   display: flex;
   flex-direction: column;
+  width: 35vw;
+  height: 80vh;
+  margin: 0vh auto;
 `
 const ViewPoint = styled.div`
   max-height: 80vh;
@@ -231,68 +188,72 @@ const ViewPoint = styled.div`
     display: none;
   }
 `
-
-const UpperBox = styled.div`
-  display: flex;
-`
-const BottomBox = styled.div`
-  ${({ theme }) => {
-    const { paddings, margins } = theme
-    return css`
-      padding: ${paddings.sm};
-      margin: ${margins.sm};
-    `
-  }}
-`
 const BoxWrapper = styled.div`
   ${({ theme }) => {
-    const { paddings, margins } = theme
+    const { fonts, paddings, margins } = theme
     return css`
       width: 100%;
-      padding: ${paddings.base};
-      margin: ${margins.sm} 0;
-      border-radius: 1rem;
-      input {
-        display: inline-block;
+      padding: ${paddings.sm} 0vw;
+      margin: ${margins.sm} 0vw;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+
+      img {
+        width: 100%;
+        height: 40vh;
+        object-fit: cover;
+        margin: ${margins.lg} auto 0vw auto;
+      }
+      p {
+        font-weight: 300;
+        text-align: right;
+      }
+
+      span {
+        padding-left: ${paddings.sm};
+        margin: ${margins.xsm} 0vw;
+        font-size: ${fonts.size.xsm};
+        font-weight: ${fonts.weight.light};
+        text-align: left;
       }
     `
   }}
 `
-
-const Title = styled.p`
+const Maintitle = styled.h5`
   ${({ theme }) => {
-    const { fonts, margins } = theme
+    const { fonts, colors, paddings, margins } = theme
     return css`
-      font-weight: ${fonts.weight.bold};
-      font-size: ${fonts.size.sm};
-      margin-bottom: ${margins.sm};
+      width: 100%;
+      padding-bottom: ${paddings.xsm};
+      margin-bottom: ${margins.base};
+      color: ${colors.main.secondary};
+      font-size: ${fonts.size.base};
     `
   }}
 `
-const SideBox = styled.div`
+const Title = styled.h5`
   ${({ theme }) => {
-    const { paddings, margins } = theme
+    const { fonts, colors, paddings, margins } = theme
     return css`
-      width: 50%;
-      background: white;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: ${paddings.sm};
-      margin: ${margins.sm};
+      width: 100%;
+      padding-bottom: ${paddings.xsm};
+      border-bottom: 1px solid ${colors.main.primary}55;
+      margin-bottom: ${margins.base};
+      color: ${colors.main.secondary};
+      font-size: ${fonts.size.base};
     `
   }}
 `
 
 const InnerBox = styled.div`
   ${({ theme }) => {
-    const { paddings, margins } = theme
+    const { margins, colors, paddings } = theme
     return css`
-      border-radius: 1rem;
       width: 100%;
-      background-color: lightgray;
-      padding: ${paddings.base};
       margin-bottom: ${margins.base};
+      padding: ${paddings.sm};
+      border-bottom: 1px solid ${colors.main.primary}55;
     `
   }}
 `
@@ -301,9 +262,6 @@ const InnerTop = styled.div`
   display: flex;
   justify-content: space-between;
 `
-const InnerTitle = styled.div``
-const InnerMessage = styled.div``
-
 const Image = styled.img`
   ${({ theme }) => {
     const { paddings, margins } = theme
