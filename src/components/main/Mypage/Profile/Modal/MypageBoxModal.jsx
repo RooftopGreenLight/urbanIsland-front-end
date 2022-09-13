9e46ae9085878ea4e5715b5a3070946b5ae71910
RookieAND <gwangin1999@naver.com>
2022-09-13 13:55:17 +0900

    Refactor : 프로필 이미지 변경 Modal 리디자인
    프로필 이미지 변경 모달 창을 리디자인하고, 관련 기능 코드를 개선했습니다.

--- src/components/main/Mypage/Profile/Modal/MypageBoxModal.jsx
+++ src/components/main/Mypage/Profile/Modal/MypageBoxModal.jsx
@@ -1,7 +1,7 @@
-import { useContext, useState } from "react"
+import { useContext, useRef, useState } from "react"
 import styled, { css } from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faXmark } from "@fortawesome/free-solid-svg-icons"
+import { faBuilding, faXmark } from "@fortawesome/free-solid-svg-icons"
 import { modalShow } from "styles/Animation"
 import { ModalContext } from "module/Modal"
 import { mypageControl } from "api/controls/mypageControl"
@@ -9,9 +9,28 @@ import defaultProfile from "assets/img/defaultProfile.png"
 
 const MypageBoxModal = ({ userData, setUserData }) => {
   const { closeModal } = useContext(ModalContext)
-  const [file, setFile] = useState()
+  const [profileImg, setProfileImg] = useState(null)
+  const [imgBase64, setImgBase64] = useState(null)
+  const fileUploadInput = useRef()
+  const feedbackMsg = useRef()
+
+  // Blob 데이터를 추출하여 이미지를 띄우는 함수.
+  const addNewImage = e => {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    setProfileImg(file)
+    reader.readAsDataURL(file)
+    reader.onloadend = () => {
+      const base64Img = reader.result
+      const base64Sub = base64Img.toString()
+      setImgBase64(base64Sub)
+    }
+  }
 
   const deleteProfile = async () => {
+    setImgBase64(null)
+    setProfileImg(null)
+    fileUploadInput.current.value = null
     try {
       await mypageControl.deleteProfile()
       setUserData({ ...userData, profile: defaultProfile })
@@ -20,9 +39,13 @@ const MypageBoxModal = ({ userData, setUserData }) => {
     }
   }
 
-  const onFinish = async () => {
+  const submitProfileImage = async () => {
+    if (!profileImg) {
+      feedbackMsg.current.innerText = "새롭게 변경할 프로필 이미지를 올려주세요."
+      return
+    }
     const formData = new FormData()
-    formData.append("file", file)
+    formData.append("file", profileImg)
     try {
       const result = mypageControl.postProfile(formData)
       setUserData({ ...userData, profile: result })
@@ -32,26 +55,37 @@ const MypageBoxModal = ({ userData, setUserData }) => {
     }
   }
 
-  const handleUpload = e => {
-    e.preventDefault()
-    setFile(e.target.files[0])
-  }
-
   return (
     <Wrapper>
-      <header>
-        <ModalCloseBtn onClick={closeModal}>
-          <FontAwesomeIcon icon={faXmark} />
-        </ModalCloseBtn>
-      </header>
+      <ModalHeader>
+        <h5>프로필 이미지 변경</h5>
+        <ModalCloseBtn icon={faXmark} onClick={closeModal} />
+      </ModalHeader>
       <ModalContent>
-        <form method="post" encType="multipart/form-data">
-          <input type="file" onChange={handleUpload} />
-        </form>
-        <button type="button" value="upload" onClick={onFinish}>
-          업로드
-        </button>
-        <button onClick={deleteProfile}>이미지 삭제</button>
+        <h5>프로필 이미지 등록하기</h5>
+        <p>새롭게 적용할 프로필 이미지를 등록합니다.</p>
+        <BtnList>
+          <label htmlFor="imgList">
+            <FileUploadBtn>사진 업로드</FileUploadBtn>
+          </label>
+          <input
+            type="file"
+            id="imgList"
+            onChange={addNewImage}
+            accept=".png, .jpg"
+            ref={fileUploadInput}
+          />
+        </BtnList>
+        {imgBase64 && <img src={imgBase64} alt="None" />}
+        <FeedBackMsg ref={feedbackMsg} />
+        <ModifyBtnList>
+          <ApplyModifyBtn onClick={submitProfileImage}>
+            <FontAwesomeIcon icon={faBuilding} /> 프로필 등록
+          </ApplyModifyBtn>
+          <ApplyModifyBtn onClick={deleteProfile}>
+            <FontAwesomeIcon icon={faXmark} /> 프로필 삭제
+          </ApplyModifyBtn>
+        </ModifyBtnList>
       </ModalContent>
     </Wrapper>
   )
@@ -82,18 +116,36 @@ const Wrapper = styled.section`
     `
   }}
 `
-const ModalCloseBtn = styled.button`
+const ModalHeader = styled.div`
   ${({ theme }) => {
-    const { fonts } = theme
+    const { colors, fonts, paddings } = theme
     return css`
-      margin: 0vw 0vw 0vw auto
+      width: 100%;
+      padding: ${paddings.base};
 
-      color: #999;
-      background-color: transparent;
+      background-color: ${colors.main.primary};
 
-      font-size: ${fonts.size.xsm};
-      font-weight: 700;
+      display: flex;
+      justify-content: space-between;
+
+      color: ${colors.white};
       text-align: center;
+
+      h5 {
+        font-size: ${fonts.size.base};
+        vertical-align: center;
+      }
+    `
+  }}
+`
+
+const ModalCloseBtn = styled(FontAwesomeIcon)`
+  ${({ theme }) => {
+    const { colors, fonts, paddings } = theme
+    return css`
+      padding: ${paddings.sm};
+      color: ${colors.white};
+      font-size: ${fonts.size.xsm};
     `
   }}
 `
@@ -111,41 +163,100 @@ const ModalContent = styled.main`
       background-color: ${colors.white};
 
       h5 {
-        margin: ${margins.base};
+        margin: ${margins.base} 0vw ${margins.xsm} 0vw;
         font-size: ${fonts.size.sm};
         text-align: center;
       }
 
-      input {
-        width: 90%;
-        padding: ${paddings.sm};
-        margin: 0vw auto ${margins.base} auto;
-
-        background-color: transparent;
-        border: 0;
-        border-bottom: 1px solid #232323;
-
-        &::placeholder {
-          color: #3e3e3e;
-          text-align: left;
-          font-weight: 100;
-        }
-
-        &::before {
-          background-color: #d9d9d9;
-        }
-      }
-
-      button {
-        width: 25%;
-        padding: ${paddings.sm};
-        margin: ${margins.base} auto;
-
-        background-color: #000000;
-        border-radius: 25px;
-        text-align: center;
-        color: ${colors.white};
+      p {
         font-size: ${fonts.size.xsm};
+        font-weight: ${fonts.weight.light};
+        text-align: center;
+      }
+    `
+  }}
+`
+
+const FileUploadBtn = styled.div`
+  ${({ theme }) => {
+    const { colors, paddings } = theme
+    return css`
+      width: 100%;
+      padding: ${paddings.sm};
+
+      border: 1px solid ${colors.main.primary};
+      border-radius: 2.5vw;
+      background: ${colors.white};
+      cursor: pointer;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      font-weight: 100;
+
+      &:hover {
+        border: 0px;
+        background: ${colors.main.tertiary};
+        color: ${colors.white};
+      }
+    `
+  }}
+`
+
+const BtnList = styled.form`
+  width: 72.5%;
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem auto 1rem auto;
+
+  label {
+    width: 100%;
+    margin: auto;
+  }
+
+  #imgList {
+    display: none;
+  }
+`
+
+const ModifyBtnList = styled.div`
+  width: 75%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+`
+
+const FeedBackMsg = styled.p`
+  ${({ theme }) => {
+    const { fonts, margins } = theme
+    return css`
+      font-size: ${fonts.size.xsm};
+      font-weight: 100;
+
+      margin: ${margins.sm} auto;
+    `
+  }}
+`
+
+const ApplyModifyBtn = styled.button`
+  ${({ theme }) => {
+    const { colors, fonts, paddings, margins } = theme
+    return css`
+      width: 45%;
+      padding: ${paddings.sm};
+      margin: ${margins.base} auto;
+
+      background-color: ${colors.main.primary};
+      border: 1px solid #7d7d7d;
+      border-radius: 25px;
+
+      color: ${colors.white};
+      text-align: center;
+      font-size: ${fonts.size.xsm};
+
+      svg {
+        margin: 0vw ${margins.xsm} 0vw auto;
       }
     `
   }}
