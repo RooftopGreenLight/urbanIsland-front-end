a44ece45c29a07b0f9f4dc833fd0f2d4846e27db
RookieAND <gwangin1999@naver.com>
2022-09-14 11:27:32 +0900

    Feat : 메인 페이지 글자 클릭 시 홈으로 이동
    메인 페이지의 Urban Island 텍스트를 클릭할 시 홈으로 이동하는 기능을 추가했습니다.

--- src/components/main/Home/Home.jsx
+++ src/components/main/Home/Home.jsx
@@ -1,16 +1,18 @@
 import styled, { css } from "styled-components"
+import { Link } from "react-router-dom"
 
 import HomeBtnList from "components/main/Home/HomeBtnList"
 import HomeBottomText from "components/main/Home/HomeBottomText"
-import Information from "components/main/Home/Information/Information"
 import bgImg2 from "assets/img/background2.jpg"
 
 const Home = () => {
   return (
     <Wrapper>
       <HomeText>
-        <h3>Urban Island</h3>
-        <p>도심 속, 우리들의 숨겨진 쉼터</p>
+        <Link to="/">
+          <h3>Urban Island</h3>
+          <p>도심 속, 우리들의 숨겨진 쉼터</p>
+        </Link>
       </HomeText>
       <HomeBtnList />
       <HomeBottomText />
