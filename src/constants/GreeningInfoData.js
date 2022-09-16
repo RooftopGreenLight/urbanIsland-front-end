767c2c0423d99591252fb4ed9801573c0c4a465d
RookieAND <gwangin1999@naver.com>
2022-09-16 16:09:19 +0900

    Feat : 옥상녹화 공모 페이지 추가
    옥상녹화 공모 기능을 하는 페이지를 추가했습니다.

---
+++ src/constants/GreeningInfoData.js
@@ -0,0 +1,41 @@
+import service1 from "assets/img/service1.jpg"
+import service2 from "assets/img/service2.jpg"
+import service3 from "assets/img/service3.jpg"
+
+export const GreeningInfoData = [
+  {
+    title: "안동시 녹색 건축물 조성 지원사업 재공고.",
+    date: "신청 기간 : 2022.6.20 (월) ~ 당해 예산 소진 시까지.",
+    tel: "담당부서 연락처 : 건축과 (054 - 840 - 5446)",
+    img: service1,
+    url: "https://www.andong.go.kr/portal/saeol/gosi/view.do?notAncmtMgtNo=45476&isLinkage=Y&mId=0401020000",
+  },
+  {
+    title: "강남구 시민주도 도시녹화 주민제안사업 공모.",
+    date: "신청 기간 : 2022.1.28 (금) ~ 2.11 (금)",
+    tel: "담당부서 연락처 : 강남구청 공원녹지과 (02 - 3423 - 6276)",
+    img: service2,
+    url: "https://www.gangnam.go.kr/notice/view.do?not_ancmt_mgt_no=43111&mid=ID05_0402",
+  },
+  {
+    title: "대구광역시 푸른옥상가꾸기사업 공모.",
+    date: "신청 기간 : 2022.1.17 (월) ~ 2.4 (금)",
+    tel: "담당부서 연락처 : 산림녹지과 (053 - 803 - 4373)",
+    img: service3,
+    url: "https://info.daegu.go.kr/newshome/mtnmain.php?mtnkey=articleview&mkey=scatelist&mkey2=2&aid=252591",
+  },
+  {
+    title: "안양시 건축물 옥상녹화 사업 공모.",
+    date: "신청 기간 : 2022.2.3 (목) ~ 2.28 (월)",
+    tel: "담당부서 연락처 : 건축과 (031 - 8045 - 5634)",
+    img: service1,
+    url: "https://www.anyang.go.kr/main/selectEminwonView.do?&notAncmtSeCode=01,04&not_ancmt_mgt_no=59299&pageUnit=10&pageIndex=1&searchCnd=B_Subject&searchKrwd=%EC%98%A5%EC%83%81%EB",
+  },
+  {
+    title: "창원시 옥상녹화 사업 공모.",
+    date: "신청 기간 : 2021.12.27 (월) ~ 12.31 (금)",
+    tel: "담당부서 연락처 : 산림휴양과 (055 - 225 - 7104)",
+    img: service2,
+    url: "https://www.changwon.go.kr/portal/bbs/view.do?bIdx=703644&ptIdx=108&mId=0201020000",
+  },
+]
